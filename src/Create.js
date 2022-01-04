13ec692ec773ff8da428668044270aed091f2bbf
shubhangsharmasigs <shubhangsharmasigs@gmail.com>
2022-01-04 13:18:02 +0530

    react router and router link

---
+++ src/Create.js
@@ -0,0 +1,7 @@
+const Create = () => {
+    return ( 
+        <h2>Add a New Blog</h2>
+     );
+}
+ 
+export default Create;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,12 @@
-
+import {Link} from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>NGs-Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-            </div>
+                <Link to="/">Home</Link>
+                <Link to="/Create">New Blog</Link>
+             </div>
         </nav>
      );
 }
